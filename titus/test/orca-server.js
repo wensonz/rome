c9e6d3c6a4a7441bbb5c5c09ee956d80c88a015e
Wenson Zhou <wenson.zhou@hotmail.com>
2013-05-14 04:35:37 -0400

    add test for titus

---
+++ titus/test/orca-server.js
@@ -0,0 +1,19 @@
+#!/usr/bin/env node
+
+var io = require('socket.io'),
+    http = require('http');
+    
+
+io = io.listen(8080);
+io.sockets.on('connection', function (socket) {
+    socket.on('exec', function (data) {
+        console.log('>>> Exec: ');
+        console.log('  * stat: ' + data.stat);
+        socket.close(function () {
+            process.exit(0);
+        });
+    });
+    socket.emit('exec', { id:123 });
+});
+
+console.log('>>> Server is running ...');
