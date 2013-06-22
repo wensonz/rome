4ea6f8d206acf7deca5539eab7f297ae0348c5b1
Wenson Zhou <wenson.zhou@gmail.com>
2013-06-22 20:32:49 +0800

    remove loading resource.template

--- augustus/src/resources/all.js
+++ augustus/src/resources/all.js
@@ -3,13 +3,9 @@
  * 
  * @module caligula.components.configuration.resources.all
  */
-Condotti.add(
-    'caligula.components.configuration.resources.all', 
-    function (C) {},
-    '0.0.1',
-    { requires: [
-        'caligula.components.configuration.resources.base',
-        'caligula.components.configuration.resources.file',
-        'caligula.components.configuration.resources.template'
-    ] }
-);
+Condotti.add('caligula.components.configuration.resources.all', function (C) {
+    //
+}, '0.0.1', { requires: [
+    'caligula.components.configuration.resources.base',
+    'caligula.components.configuration.resources.file'
+]});
