b334ce08b2f675b394cc35b9af6933906ba80aae
Wenson Zhou <wenson.zhou@gmail.com>
2013-06-22 19:49:19 +0800

    add component.js and .json for mozart

---
+++ mozart/src/component.js
@@ -0,0 +1,10 @@
+/**
+ * The entrance module for the orchestration component
+ *
+ * @module caligula.components.orchestration.component
+ */
+Condotti.add('caligula.components.orchestration.component', function (C) {
+    
+    //
+    
+}, '0.0.1', { requires: ['caligula.components.orchestration.base'] });
